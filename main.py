4a58004a5a8142c067b000516e70a2819db677a5
Michael Ogaye <hogaeymic@gmail.com>
2022-04-23 18:10:41 +0300

    defination of the cr4edential init object

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class Users:
 
     def delete_user(self):
         '''
-        delete_account method deletes a  saved account from the list
+        delete_account method deletes a  user
         '''
         Users.user_list.remove(self)
     
@@ -45,3 +45,22 @@ class Credentials:
     """
     this is a blue print of crendetials object
     """
+    credslist=[]
+    @classmethod
+    def verify_user(cls,username, password):
+        """
+        method to verify whether the user is in our user_list or not
+        """
+        a_user = ""
+        for user in Users.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
+
+    def __init__(self,account,userName, password):
+        """
+        instantiation of the credential class object
+        """
+        self.account = account
+        self.userName = userName
+        self.password = password
