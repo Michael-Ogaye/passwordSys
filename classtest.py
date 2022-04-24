6a7011ad7ee893a6ffd4643b4e2e2ef2ca05a14b
Michael Ogaye <hogaeymic@gmail.com>
2022-04-24 10:27:14 +0300

    initialization of user and credential  classes for testing

--- classtest.py
+++ classtest.py
@@ -2,7 +2,7 @@ import unittest
 from main import Users
 from main import Credentials
 
-class TestClass(unittest.TestCase):
+class Testuser(unittest.TestCase):
     """
     A Test class that defines test cases for the User class.
     """
@@ -18,4 +18,31 @@ class TestClass(unittest.TestCase):
         """
         self.assertEqual(self.new_user.username,'OgayeMike')
         self.assertEqual(self.new_user.password,'yuvicytr')
+
+    def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+        """
+        self.new_user.adduser()
+        self.assertEqual(len(Users.user_list),1)
+
+class TestCreds(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+    """ 
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+        """
+        self.new_credential = Credentials('uma','Ogaye Michael','beaut56')
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account,'uma')
+        self.assertEqual(self.new_credential.userName,'Ogaye Michael')
+        self.assertEqual(self.new_credential.password,'beaut56')
+
+    
+
 
