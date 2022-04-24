d1ce97021944b7aa482a71cc3bddb00e225750d4
Michael Ogaye <hogaeymic@gmail.com>
2022-04-24 10:11:09 +0300

    import of the unittest module

--- classtest.py
+++ classtest.py
@@ -1,3 +1,14 @@
 import unittest
 from main import Users
 from main import Credentials
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = Users('Ogaye Mike','yuvicytr')
+
