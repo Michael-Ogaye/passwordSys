ce3232c42972c2e625dac8e477d0befb41a0bcc9
Michael Ogaye <hogaeymic@gmail.com>
2022-04-24 10:17:23 +0300

    ussing the assertequal method to confirm equality status

--- classtest.py
+++ classtest.py
@@ -10,5 +10,12 @@ class TestClass(unittest.TestCase):
         """
         Method that runs before each individual test methods run.
         """
-        self.new_user = Users('Ogaye Mike','yuvicytr')
+        self.new_user = Users('OgayeMike','yuvicytr')
+    
+    def test_init(self):
+        """
+        test case to chek if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username,'OgayeMike')
+        self.assertEqual(self.new_user.password,'yuvicytr')
 
